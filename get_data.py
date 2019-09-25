a3881e1696f9dc663a7e2e4dd40128009cac2598
Brianna Griffin <briannagriffin@Briannas-MacBook-Pro.local>
2019-09-24 23:11:02 -0600

    add read_stdin_col method

--- get_data.py
+++ get_data.py
@@ -1,3 +1,31 @@
+import sys
+
 
 def read_stdin_col(col_num):
-    return None
+    file_name = sys.stdin
+
+    try:
+        f = open(file_name)
+    except FileNotFoundError:
+        raise FileNotFoundError(file_name + ' not found.')
+    num = 0
+    L = []
+
+    for l in f:
+        A = l.rstrip().split(',')
+        try:
+            L.append(int(A[col_num]))
+        except IndexError:
+            f.close()
+            return None
+        except ValueError:
+            f.close()
+            return None
+        num += 1
+
+    f.close()
+
+    if num == 0:
+        return None
+
+    return L
