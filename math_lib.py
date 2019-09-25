42cdb6d97509d15563fa4ac3e2fb2db3ae36cd93
Brianna Griffin <briannagriffin@Briannas-MacBook-Pro.local>
2019-09-24 21:32:05 -0600

    add list_stdev method

--- math_lib.py
+++ math_lib.py
@@ -1,3 +1,4 @@
+import math
 
 def list_mean(L):
     if L is None:
@@ -9,11 +10,27 @@ def list_mean(L):
     sum = 0
     for l in L:
         if not isinstance(l, int) and not isinstance(l, float):
-            raise ValueError('Mean undefined. Unsuported value in list.')
+            raise ValueError('Mean undefined. Unsupported value in list.')
         sum += l
 
     return sum/len(L)
 
 
 def list_stdev(L):
-    return None
+    #math.sqrt(sum([(m-x)**2 for x in L]) / len(L))
+    try:
+        m = list_mean(L)
+    except ValueError:
+        raise ValueError('Stdev undefined. Unsupported value in list.')
+
+    if m is None:
+        return None
+
+    if len(L) == 1:
+        return None
+
+    sum = 0
+    for l in L:
+        sum += (m - l) ** 2
+
+    return math.sqrt(sum/(len(L)-1))
